812c11028e02e49d8b27539338f136c1a99e77d6
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 09:41:15 +0200

    day02/ex03

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+void ft_putchar(char c);
+/*{
+	write(1, &c, 1);
+}*/
+void ft_is_negative(int n)
+{
+	if(n > 0)
+		ft_putchar('P');
+	else
+		ft_putchar('N');
+}
+/*int main(int n, char *c[])
+{
+	/*char p;
+	int i;
+	int s;
+
+	i = 0;
+	p = ' ';
+	while(i < sizeof(c))
+		p = p + c[i];
+		s = s + p;
+
+	char *p = c[1];
+	int passd = atoi(p);
+
+	ft_is_negative(passd);
+
+	return(0);
+}*/
