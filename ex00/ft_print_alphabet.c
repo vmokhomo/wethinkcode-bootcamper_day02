4d98d1f0b3482d4b08ce25133c0836f68ac39fbd
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 09:39:47 +0200

    day02/ex00

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void ft_putchar(char c);
+/*{
+	write(1, &c, 1);
+}*/
+void ft_print_alphabet(void)
+{
+	char l;
+	char l2;
+	//int i;
+
+	//i = 0;
+	l2 = 'z';
+	l = 'a';
+	while(l <= l2)
+	{
+		ft_putchar(l);
+		l++;
+	}
+}
+/*int main()
+{
+	ft_print_alphabet();
+
+	return(0);
+}*/
