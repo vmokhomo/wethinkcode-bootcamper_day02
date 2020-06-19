feb761a700d599d4987141f2bcb08e322d2b3e94
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 09:40:17 +0200

    day02/ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void ft_print_reverse_alphabet(void)
+{
+	char l;
+	char l2;
+
+	l = 'z';
+	l2 = 'a';
+	while(l >= l2)
+	{
+		ft_putchar(l);
+		l--;
+	}
+}
+int main()
+{
+	ft_print_reverse_alphabet();
+
+	return(0);
+}
