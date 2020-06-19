60315e92527500d8a4a725751167a95e1b70de52
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 09:40:47 +0200

    day02/ex02

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void ft_putchar(char c);
+/*{
+	write(1, &c, 1);
+}*/
+void ft_print_numbers(void)
+{
+	int i;
+	int i1;
+
+	i1 = '9';
+	i = '0';
+
+	while(i <= i1)
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
+/*int main()
+{
+	ft_print_numbers();
+
+	return(0);
+}*/
